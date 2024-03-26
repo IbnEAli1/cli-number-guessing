e7bc464dfbd627ee96989ad8b4c8c75137ae2c8d
simple_calculator <you@example.com>
2024-03-27 00:05:18 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+//shabang
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userguessNumber",
+        type: "number",
+        message: "Please guess a number (1-6): ",
+    },
+]);
+//condition
+if (answers.userguessNumber === randomNumber) {
+    console.log("Congratuations; Your guess a right number.");
+}
+else {
+    console.log("You guessed a wrong number.");
+}
